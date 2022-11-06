bbfba792f68c4e1fb6b49fbcf793117cb21bf06b
Isotoxal <62714538+IsotoxalDev@users.noreply.github.com>
2022-11-06 15:04:57 +0530

    Move tests to a separate file

--- src/lib.rs
+++ src/lib.rs
@@ -8,32 +8,3 @@ enum HtmlData {
     Comment(String),
     PlainText(String),
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::Element;
-    #[test]
-    fn self_closing () {
-        let el = Element::new("H1", true);
-        assert_eq!(format!("{}", el), "<H1 />")
-    }
-    #[test]
-    fn non_self_closing () {
-        let el = Element::new("Div", false);
-        assert_eq!(format!("{}", el), "<Div>\n</Div>")
-    }
-    #[test]
-    fn comment () {
-        let mut el = Element::new("Div", false);
-        el.add_comment("A Comment");
-        assert_eq!(format!("{}", el), "<Div>\n  <-- A Comment -->\n</Div>")
-    }
-    #[test]
-    fn child () {
-        let mut el = Element::new("Div", false);
-        let mut h1 = Element::new("H1", false);
-        h1.add_text("Text");
-        el.add_element(h1);
-        assert_eq!(format!("{}", el), "<Div>\n  <H1>\n    Text\n  </H1>\n</Div>")
-    }
-}
